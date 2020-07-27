3a985c2240f0ee2fcbace0e47dd50bb234171423
Justin Lohner <lohner4@gmail.com>
2020-07-27 16:21:15 -0600

    wrote cell class

---
+++ game/cell.py
@@ -0,0 +1,16 @@
+class Cell:
+	def __init__(self, isAlive=False, isBlack=False):
+		self.isAlive = isAlive
+		self.isBlack = isBlack
+		self.clickable = True
+
+	def __repr__(self):
+		# return f'isAlive - {self.isAlive} | isBlack - {self.isBlack} | clickable - {self.clickable}'
+		return f'{1 if self.isAlive else 0}'
+
+	def toggleAlive(self):
+		self.isAlive = (not self.isAlive)
+		self.isBlack = (not self.isBlack)
+
+	def toggleClickable(self):
+		self.clickable = (not self.clickable)
