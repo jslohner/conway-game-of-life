e2e070b383c8a1ca4618743c54e444afa41944f4
Justin Lohner <lohner4@gmail.com>
2020-07-29 12:33:26 -0600

    still working on styling grid and building react page

--- game-of-life/src/app/components/Grid/Cell.js
+++ game-of-life/src/app/components/Grid/Cell.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Cell({ cellNum }) {
+	let [isAlive, setIsAlive] = useState(false);
+	let cellState = isAlive ? 'alive' : 'dead'
+
+	const toggleAlive = () => {
+		setIsAlive(!isAlive);
+	}
+
 	return (
-		<div className='grid-cell'>
-			{/* <p>{cellNum}</p> */}
-			<p></p>
+		<div className={`grid-cell-${cellState}`} onClick={toggleAlive}>
+			{/* <p></p> */}
 		</div>
 	);
 }
